39321ab6172daa4a997a2ec5375239d3279d7648
Jansen McQuivey <jansen@lucidchart.com>
2015-08-12 14:47:05 -0600

    Progress on Lucid Laptop

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Divine Comedy' });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/shows', function(req, res) {
+  res.render('shows', {title: 'Shows'});
+});
+
+router.get('/auditions', function(req, res) {
+  res.render('auditions', {title: 'Auditions'});
+});
+
+router.get('/cast-crew', function(req, res) {
+  res.render('cast-crew', {title: 'Cast & Crew'});
+});
+
+router.get('/faq', function(req, res) {
+  res.render('faq', {title: 'FAQ'});
+});
+
+router.get('/media', function(req, res) {
+  res.render('media', {title: 'Media'});
+});
+
+router.get('/tickets', function(req, res) {
+  res.render('tickets', {title: 'Tickets'});
+});
+
+router.get('/admin', function(req, res) {
+  res.render('admin');
 });
 
 router.get('/test', function(req, res) {
